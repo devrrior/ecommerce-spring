0ff75c8503adfed57a8dd1009224e1f258a47505
Fernando Guerrero <devrrior@gmail.com>
2022-10-11 13:17:21 -0500

    feat(repositories/IAddressRepository): add findAllByUserId method

--- src/main/java/com/devrrior/ecommercespring/repositories/IAddressRepository.java
+++ src/main/java/com/devrrior/ecommercespring/repositories/IAddressRepository.java
@@ -4,6 +4,9 @@ import com.devrrior.ecommercespring.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IAddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findAllByUserId(Long userId);
 }
